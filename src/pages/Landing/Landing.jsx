f97bb59dddbeea303d5bfbfbfeffa5c43b38c1da
AdityaChaudhary0005 <chaudharyaditya0005@gmail.com>
2024-09-05 23:26:36 +0530

    added link

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -17,7 +17,7 @@ export default function Landing() {
 
   const handleMessage = async (e) => {
     //prevent page reload due to form submit
-    if(isSending) return;
+    if (isSending) return;
 
     e.preventDefault();
     const address = document.getElementById("address").value;
@@ -34,7 +34,7 @@ export default function Landing() {
           body: JSON.stringify({
             message: "MESSAGE FROM CYEBR GLADIATOR",
             id: username,
-            username: address ,
+            username: address,
           }),
         });
         if (response.status === 200) {
@@ -52,55 +52,66 @@ export default function Landing() {
     //make a post request to the server
   };
   return (
-    <div className="bg-black">
+    <div className='bg-black'>
       <>
-        <div className="pt-5 sm:pt-2 bg-black">
-          <div className="container px-3 pt-4 mx-auto flex flex-wrap flex-col md:flex-row items-center pb-8 ">
-            <div className="flex flex-col w-full md:w-2/5 justify-center items-start text-center md:text-left ">
-              <div className="mx-auto">
-                <h2 className="text-white ">
-                  <p className=" text-white text-4xl sm:text-5xl">CYBER</p>
-                  <p className=" text-whit text-4xl sm:text-5xl mt-3">GLADIATORS</p>
-                  <p className="m-0 text-white">&nbsp;</p>
+        <div className='pt-5 sm:pt-2 bg-black'>
+          <div className='container px-3 pt-4 mx-auto flex flex-wrap flex-col-reverse md:flex-row items-center pb-8 '>
+            <div className='flex flex-col w-full md:w-2/5 justify-center items-start text-center md:text-left '>
+              <div className='mx-auto'>
+                <h2 className='text-white '>
+                  <p className=' text-white text-4xl sm:text-5xl'>CYBER</p>
+                  <p className=' text-whit text-4xl sm:text-5xl mt-3'>
+                    GLADIATORS
+                  </p>
+                  <p className='m-0 text-white'>&nbsp;</p>
                 </h2>
 
-                <div className="text-white text-2xl mt-1 ml-2">Theme Based NFT</div>
+                <div className='text-white text-2xl mt-1 ml-2'>
+                  Theme Based NFT
+                </div>
+                <div className="ml-2 mt-7">
+                  <a
+                    href='https://opensea.io/collection/cyber-gladiator'
+                    target="_blank"
+                    className='bg-red-500 hover:bg-red-600 text-white rounded-lg px-5 py-3 text-xl  '>
+                    View Collection
+                  </a>
+                </div>
               </div>
-              <div className="w-full flex-grow lg:flex lg:items-center lg:w-auto mt-2 lg:mt-0  lg:bg-transparent  p-4 lg:p-0 z-20"></div>
+              <div className='w-full flex-grow lg:flex lg:items-center lg:w-auto mt-2 lg:mt-0  lg:bg-transparent  p-4 lg:p-0 z-20'></div>
               {/* <Login /> */}
             </div>
 
-            <div className="px-2 md:w-3/6  flex justify-center my-8 sm:my-5">
-              <img className=" " src={union} />
+            <div className='px-2 md:w-3/6  flex justify-center my-8 sm:my-5'>
+              <img className=' ' src={union} />
             </div>
           </div>
         </div>
-        <div className="bg-[#0f0f0f] py-10">
-          <div className="flex justify-center mx-auto flex-col  w-3/4 sm:w-3/6 space-y-4 items-center ">
-            <p className="text-white text-2xl text-center ">
+        <div className='bg-[#0f0f0f] py-10'>
+          <div className='flex justify-center mx-auto flex-col  w-3/4 sm:w-3/6 space-y-4 items-center '>
+            <p className='text-white text-2xl text-center '>
               JOIN WAITING LIST
             </p>
 
-            <p className="text-white">Avax Address</p>
+            <p className='text-white'>Avax Address</p>
             <input
-              type="text"
-              id="address"
-              placeholder="0x..."
-              className="bg-black text-white border-2 rounded-lg p-2 outline-none border-[#1f1f1f] w-full"
+              type='text'
+              id='address'
+              placeholder='0x...'
+              className='bg-black text-white border-2 rounded-lg p-2 outline-none border-[#1f1f1f] w-full'
             />
 
-            <p className="text-white">Arena Username</p>
+            <p className='text-white'>Arena Username</p>
             <input
-              type="text"
-              id="username"
-              placeholder="@AvaxUsername"
-              className="bg-black text-white border-2 rounded-lg p-2 outline-none border-[#1f1f1f] w-full "
+              type='text'
+              id='username'
+              placeholder='@AvaxUsername'
+              className='bg-black text-white border-2 rounded-lg p-2 outline-none border-[#1f1f1f] w-full '
             />
 
             <button
-              className="bg-red-500 hover:bg-red-600 text-white rounded-lg px-4 py-2  "
-              onClick={handleMessage}
-            >
+              className='bg-red-500 hover:bg-red-600 text-white rounded-lg px-4 py-2  '
+              onClick={handleMessage}>
               Submit
             </button>
           </div>
